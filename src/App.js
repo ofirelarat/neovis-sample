1670a9022c50b6ecb9fb591315d3e4382c945bcd
Itamar Dori <itamardori@local>
2020-05-13 17:53:24 +0300

    added dynamic query for expanding child relations

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import NeoVis from 'neovis.js';
 
 import './App.css';
 
+function buildQuery(matches, where, returns) {
+  return `MATCH ${matches.join(', ')}  ${where.length > 0 ? `WHERE `+ where.join(' AND '): ""} RETURN ${returns.join(', ')}`
+}
+
 function App() {
   useEffect(() => {
     var viz;
-    
+    let matches = [`(tom {name: "Tom Hanks"})`];
+    let where = [];
+    let returns = [`tom`];
+    let query = buildQuery(matches, where,returns);
     var config = {
       container_id: "viz",
       server_url: "bolt://neo4j-tira-host.apps.ocp4.idf-cts.com:443",
@@ -31,11 +38,27 @@ function App() {
           }
       },
       arrows: true,
-      initial_cypher: "MATCH (n) RETURN n"
+      initial_cypher: query
     };
 
     viz = new NeoVis(config);
     viz.render();
+
+    viz.registerOnEvent("completed", (e)=>{ 
+      viz["_network"].on("click", (event)=>{ 
+        if(event.nodes[0] !== undefined) {
+          const id = matches.length;
+          matches.push(`p${id} = (n${id})-[*1..1]-()`);
+          where.push(`ID(n${id})=${event.nodes[0]}`);
+          returns.push(`p${id}`)
+          
+          console.log(buildQuery(matches, where,returns))
+
+
+          viz.renderWithCypher(buildQuery(matches, where,returns));
+        }
+      });
+  });
 
   },[]);
 

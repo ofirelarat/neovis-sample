af0d9615a075267e6f32d1a2d34d3668552a57fc
Itamar Dori <itamardori@local>
2020-05-18 16:19:28 +0300

    update with custom neovis version

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
 import React, { useEffect } from 'react';
-import NeoVis from 'neovis.js';
+import NeoVis from './neovis/neovis';
 
 import './App.css';
-
-function buildQuery(matches, where, returns) {
-  return `MATCH ${matches.join(', ')}  ${where.length > 0 ? `WHERE `+ where.join(' AND '): ""} RETURN ${returns.join(', ')}`
-}
 
 function App() {
   useEffect(() => {
     var viz;
-    let matches = [`(tom {name: "Tom Hanks"})`];
-    let where = [];
-    let returns = [`tom`];
-    let query = buildQuery(matches, where,returns);
+    let query = 'MATCH (tom {name: "Tom Hanks"}) RETURN tom';
     var config = {
       container_id: "viz",
       server_url: "bolt://neo4j-tira-host.apps.ocp4.idf-cts.com:443",
       server_user: "neo4j",
-      server_password: "tira",
+      server_password: "balash123",
       encrypted: "ENCRYPTION_ON",
       trust: "TRUST_ALL_CERTIFICATES",
       labels: {
-          "PERSON": {
-              "caption": "NAME",
-              "size": 1.0,
-              "community": "community",
-              "title_properties": [
-                  "NAME",
-              ]
+          "Person": {
+              "caption": "name",
+          },
+          "Movie": {
+            "caption": "title"
           }
       },
       relationships: {
-          "BROTHERS": {
+          "ACTED_IN": {
               "thickness": "weight",
               "caption": true
-          }
+          },
+          "DIRECTED": {
+            "thickness": "weight",
+            "caption": true
+        }
       },
       arrows: true,
       initial_cypher: query
@@ -47,15 +42,11 @@ function App() {
     viz.registerOnEvent("completed", (e)=>{ 
       viz["_network"].on("click", (event)=>{ 
         if(event.nodes[0] !== undefined) {
-          const id = matches.length;
-          matches.push(`p${id} = (n${id})-[*1..1]-()`);
-          where.push(`ID(n${id})=${event.nodes[0]}`);
-          returns.push(`p${id}`)
-          
-          console.log(buildQuery(matches, where,returns))
+          query =`
+          MATCH p = (n)-[*1..1]-() WHERE ID(n)=${event.nodes[0]}
+          RETURN p`;
 
-
-          viz.renderWithCypher(buildQuery(matches, where,returns));
+          viz.updateWithCypher(query);
         }
       });
   });
